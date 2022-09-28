1a897b63675417ec2228383fdac0c2b6a064731e
Sacha Dumay <dumaysacha@gmail.com>
2022-09-27 22:11:40 -0500

    add exit_logic

--- backtesting/backtesting.py
+++ backtesting/backtesting.py
@@ -955,9 +955,9 @@ class _Broker:
 
     def _close_trade(self, trade: Trade, price: float, time_index: int):
         self.trades.remove(trade)
-        print(f'trade._sl_order= {trade._sl_order}')
-        print(f'trade._sp_order= {trade._tp_order}')
-        if trade._sl_order and not trade._tp_order:
+        print(f'trade._sl_order= {trade._sl_order.stop}')
+        print(f'trade._sp_order= {trade._tp_order.stop}')
+        if trade._sl_order.stop and not trade._tp_order:
             exit_logic = 'sp'
         elif not trade._sl_order and trade._tp_order:
             exit_logic = 'sl'
